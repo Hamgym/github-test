9b1136ea11f3b6cd1317824f4f63b4d8645abbad
Hamgym <pipiyakas@gmail.com>
2025-03-18 18:30:29 +0800

    更新

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -9,3 +9,26 @@ let rightBtn = document.querySelector("button.right");
 rightBtn.addEventListener("click", function () {
 	list.scrollBy({ left: 100, behavior: "smooth" });
 });
+
+let signinBtn = document.querySelector("div.signin");
+// console.log(loginBtn);
+let mask = document.querySelector("div.mask");
+let signinDialog = document.querySelector("div.signin-dialog");
+// console.log(mask);
+signinBtn.addEventListener("click", function () {
+	mask.style.display = "block";
+	signinDialog.style.display = "block";
+});
+window.addEventListener("keydown", function (ev) {
+	if (ev.key == "Escape") {
+		mask.style.display = "none";
+		signinDialog.style.display = "none";
+	}
+});
+let closeBtn = document.querySelector("div.close");
+// console.log(closeBtn);
+closeBtn.addEventListener("click", function () {
+	// console.log("HI");
+	mask.style.display = "none";
+	signinDialog.style.display = "none";
+});
