9a1afff9920c172d264b3df28572b3d52f28b1a0
Hamgym <pipiyakas@gmail.com>
2025-03-19 20:24:28 +0800

    更新

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -43,3 +43,72 @@ window.addEventListener("keydown", function (ev) {
 	}
 });
 
+
+let nextPage = 0;
+let keyword = "";
+async function init() {
+	let res = await fetch("./api/mrts");
+	let data_raw = await res.json();
+	let mrts = data_raw.data;
+	let list = document.querySelector("div.list-item");
+	let div_proto = list.firstElementChild;
+	let searchBtn = document.querySelector(".slogan button");
+	if (list.children.length == 1) {
+		for (let mrt of mrts) {
+			let div = div_proto.cloneNode(true);
+			div.style.display = "block";
+			div.innerText = mrt;
+			let searchField = document.querySelector(".slogan input");
+			div.addEventListener("click", function () {
+				searchField.value = div.innerText;
+				searchBtn.click();
+			});
+			list.appendChild(div);
+		}
+	}
+	nextPage = await loadOne(nextPage, keyword);
+}
+init();
+const intersectionObserver = new IntersectionObserver(async (entries) => {
+	if (entries[0].intersectionRatio <= 0 || nextPage == null || nextPage == 0) {
+		return;
+	}
+	nextPage = await loadOne(nextPage, keyword);
+});
+intersectionObserver.observe(document.querySelector("div.footer"));
+
+async function loadOne(page, keyword) {
+	let res = await fetch(`./api/attractions?page=${page}&keyword=${keyword}`);
+	let data_raw = await res.json();
+	let nextPage = data_raw.nextPage;
+	let data_page = data_raw.data;
+	let proto_attraction = document.querySelector("div.attraction");
+	let attractions = document.querySelector("div.attractions");
+	for (let data of data_page) {
+		let attraction = proto_attraction.cloneNode(true);
+		attraction.style.display = "block";
+		let name = attraction.querySelector("p.name");
+		name.innerText = data.name;
+		let category = attraction.querySelector("p.category");
+		category.innerText = data.category;
+		let image = attraction.querySelector("img");
+		image.setAttribute("src", data.images[0]);
+		let mrt = attraction.querySelector("p.mrt");
+		mrt.innerText = data.mrt;
+		attractions.appendChild(attraction);
+	}
+	return nextPage
+}
+
+let searchBtn = document.querySelector(".slogan button");
+searchBtn.addEventListener("click", async function () {
+	let searchField = document.querySelector(".slogan input");
+	keyword = searchField.value;
+	nextPage = 0;
+	let attractions = document.querySelector("div.attractions");
+	while (attractions.children.length > 1) {
+		attractions.removeChild(attractions.lastElementChild);
+	}
+	await init();
+});
+
